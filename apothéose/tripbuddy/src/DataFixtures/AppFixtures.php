8d0bc0d40bb73f9c2371b03bc7de3841272975be
YoanBuscail <yoan.buscail@gmail.com>
2023-09-22 17:11:01 +0200

    entity itinerary + step + category et fixtures

--- apothéose/tripbuddy/src/DataFixtures/AppFixtures.php
+++ apothéose/tripbuddy/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,104 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Category;
+use App\Entity\Itinerary;
+use App\Entity\Step;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    /**
+     * Fonction qui va s'executer quand on va charger les fixtures (envoyer les données en bdd)
+     *
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        //  ! USERS
+        $admin = new User();
+        $admin->setFirstname($faker->word());
+        $admin->setLastname($faker->word());
+        $admin->setEmail("admin@oclock.io");
+        $admin->setRoles(["ROLE_ADMIN"]);
+        // ici j'utilise le passwordhasher pour hasher le mot de passe par rapport à mes infos dans le security.yaml
+        // ! SI PAS DE HASH, L'auth ne peut pas marcher
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, "admin"));
+
+        $manager->persist($admin);
+
+        $user = new User();
+        $user->setEmail("user@oclock.io");
+        $user->setFirstname($faker->word());
+        $user->setLastname($faker->word());
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->passwordHasher->hashPassword($admin, "user"));
+
+        $manager->persist($user);
+
+        $categoryList = [];
+
+        // Boucle 10 fois pour créer 10 categories
+        for ($c = 1; $c <= 10; $c++) {
+            // on crée une entité
+            $category = new Category();
+
+            $category->setName($faker->word());
+
+            // on l'ajoute à notre tableau $categoryList[]
+            $categoryList[] = $category;
+
+            // on persist l'entité
+            $manager->persist($category);
+        }
+
+        $stepList = [];
+
+        for ($s = 1; $s <= 10; $s++) {
+            $step = new Step();
+            $step->setName($faker->word());
+            $step->setLatitude($faker->randomFloat(6, 0, 80));
+            $step->setLongitude($faker->randomFloat(6, 0, 80));
+            $step->setDescription(($faker->paragraph()));
+            $stepList [] = $step;
+
+            for ($g = 1; $g <= mt_rand(1, 3); $g++) {
+                $step->addCategory($categoryList[mt_rand(1, 9)]);
+            }
+
+            $manager->persist($step);
+        }
+
+        // On boucle 10 fois pour créer 10 itinéraires
+        for ($i = 1; $i <= 10; $i++) {
+
+            $itinerary = new Itinerary();
+            $itinerary->setTitle($faker->word(3));
+            $itinerary->setStartDate(new \DateTimeImmutable($faker->date()));
+            $itinerary->setEndDate(new \DateTimeImmutable($faker->date()));
+
+            $itinerary->setUser($user);
+
+            for ($s = 1; $s <= mt_rand(1,3); $s++) {
+                $itinerary->addStep($stepList[mt_rand(1,9)]);
+              }
+
+            $manager->persist($itinerary);
+        }
 
         $manager->flush();
     }
--- apothéose/tripbuddy/src/Entity/User.php
+++ apothéose/tripbuddy/src/Entity/User.php
@@ -3,12 +3,19 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity("email")
  */
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -18,7 +25,7 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
@@ -33,6 +40,7 @@ class User
     private $lastname;
 
     /**
+     * @var string The hashed password
      * @ORM\Column(type="string", length=255)
      */
     private $password;
@@ -41,6 +49,16 @@ class User
      * @ORM\Column(type="json")
      */
     private $roles = [];
+
+    /**
+     * @ORM\OneToMany(targetEntity=Itinerary::class, mappedBy="user")
+     */
+    private $itinerary;
+
+    public function __construct()
+    {
+        $this->itinerary = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -95,9 +113,34 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+     /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
     public function getRoles(): ?array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): self
@@ -105,5 +148,55 @@ class User
         $this->roles = $roles;
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, Itinerary>
+     */
+    public function getItinerary(): Collection
+    {
+        return $this->itinerary;
+    }
+
+    public function addItinerary(Itinerary $itinerary): self
+    {
+        if (!$this->itinerary->contains($itinerary)) {
+            $this->itinerary[] = $itinerary;
+            $itinerary->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItinerary(Itinerary $itinerary): self
+    {
+        if ($this->itinerary->removeElement($itinerary)) {
+            // set the owning side to null (unless already changed)
+            if ($itinerary->getUser() === $this) {
+                $itinerary->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }

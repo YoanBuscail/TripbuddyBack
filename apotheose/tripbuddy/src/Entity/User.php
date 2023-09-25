e6a111e5aae4ad23cb3dece51013f72d99860cab
Karine-Ollivier <karine.ollivier@oclock.school>
2023-09-25 14:03:06 +0200

    modification de make:entity user par make:user

--- apotheose/tripbuddy/src/Entity/User.php
+++ apotheose/tripbuddy/src/Entity/User.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\UserRepository;
@@ -33,7 +32,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string", length=255)
      */
     private $firstname;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
@@ -44,7 +42,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string", length=255)
      */
     private $password;
-
     /**
      * @ORM\Column(type="json")
      */
@@ -64,52 +61,40 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->id;
     }
-
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
         return $this;
     }
-
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
-
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
-
         return $this;
     }
-
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
-
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
-
         return $this;
     }
-
     public function getPassword(): ?string
     {
         return $this->password;
     }
-
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
         return $this;
     }
 
